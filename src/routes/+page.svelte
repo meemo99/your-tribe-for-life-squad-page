9c2d89819a0f1919b4efa1c8de55cba35496b6ea
meemo99 <oumaima.el.fakir@hva.nl>
2026-09-03 14:19:57 +0200

    #3 inhoud overzichtspagina

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <script>
     let { data } = $props();
 
     const persons = data.persons;
 </script>
+
+<h1>Squadpage Squad G</h1>
+
+{#each persons as person}
+    <a href="/"> {person.name}</a><br />
+{/each}
