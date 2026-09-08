a5de959d9356f240ced76e88861585eccc12c159
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-08 11:02:24 +0200

    Fix: ik haalde perongeluk de data van alle studenten op ik haal nu alleen de id op met de parameters die van belang zijn #18

--- src/routes/detail/+page.server.js
+++ src/routes/detail/+page.server.js
@@ -1,17 +1,17 @@
-export async function load() {
+export async function load(){
+    // Haalt alleen de id op via een API
+    const personRes = await fetch (`https://fdnd.directus.app/items/person?${params.id}`)
+    // Zet de API-response om naar JSON
+    const personData = await personRes.json();
+    
+    // Bepaalt welke gegevns van de student uit de API worden opgehaald 
     const params = new URLSearchParams({
-        fields: 'name,team_s13,bio,profilecard',
-        'filter[squads][squad_id][cohort][_eq]': '2627',
-        'sort': 'name'
-    });
-    const res = await fetch(
-        `https://fdnd.directus.app/items/person?${params.toString()}`
-    );
+        fields: 'name,team_s13,bio,profilecard'
+    })
 
-    const data = await res.json();
-
-    return {
-        persons: data.data
-    };
+    // Geeft de gegevens van de student door aan de pagina
+    return{
+        person: personData.data
+    }
 }
 
