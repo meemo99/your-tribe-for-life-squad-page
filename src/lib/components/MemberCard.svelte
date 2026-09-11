93cd484e104918a6a4bdc0ca460f1092da331750
Akikosophia <asschermer@hotmail.com>
2026-09-11 10:14:01 +0200

    Feat: add member mugshot to MemberCard

--- src/lib/components/MemberCard.svelte
+++ src/lib/components/MemberCard.svelte
@@ -3,34 +3,35 @@
 </script>
 
 <article class="card">
+  <header class="header">
+    <div class="window-icons">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </header>
 
-<header class="header">
-<div class="window-icons">
-    <span></span>
-    <span></span>
-    <span></span>
-</div>
-</header>
+  <div class="middle">
+    <div class="sidebar">
+      <p class="team-name"></p>
+    </div>
 
-<div class="middle"></div>
+    <img
+    class="mugshots"
+    src={`https://fdnd.directus.app/assets/${person.mugshot_year2}`}
+    alt={`Mugshot of ${person.name}`}
+    width="300"
+    height="300"
+  />
 
-<div class="sidebar">
-    <p class="team-name"> </p>
-</div>
-
-<img>
-
-<div class="right-sidebar">
-
-</div>
+    <div class="right-sidebar"></div>
+  </div>
 
   <h3>
     <a href="/{person.id}">
       {person.name}
     </a>
   </h3>
-
-
 </article>
 
 <style>
@@ -38,5 +39,9 @@
     width: 300px;
     height: 300px;
     background-color: pink;
+  }
+
+  .mugshots {
+    object-fit: cover;
   }
 </style>
