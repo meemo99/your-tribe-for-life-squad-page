a248a80ee5e9d18950f1c00e3e1384ee12f164d4
Akikosophia <asschermer@hotmail.com>
2026-09-11 10:01:05 +0200

    Feat: add MemberCard component to squad page

---
+++ src/lib/components/MemberCard.svelte
@@ -0,0 +1,30 @@
+<script>
+  let { person } = $props();
+</script>
+
+<article>
+
+<div class="card">
+
+    <ul class="icons">
+        <li class="icon"></li>
+        <li class="icon"></li>
+        <li class="icon"></li>
+    </ul>
+
+  <h3>
+    <a href="/{person.id}">
+      {person.name}
+    </a>
+  </h3>
+
+</div>
+</article>
+
+<style>
+  article {
+    width: 300px;
+    height: 300px;
+    background-color: pink;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
     let { data } = $props();
 
     const persons = $derived (data.persons);
+
+    import MemberCard from '$lib/components/MemberCard.svelte';
 </script>
 
 <h1>Squadpage Squad G</h1>
 
 {#each persons as person}
-    <a href="/{person.id}"> {person.name}</a><br />
+   <MemberCard {person} />
 {/each}
