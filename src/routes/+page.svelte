1d56f30ae242e13db0713ff90433d9145249a399
meemo99 <oumaima.el.fakir@hva.nl>
2026-09-11 20:33:11 +0200

    {#if} statement togevoegd
    #24

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,18 @@
 {#each persons as person}
     <a href="/">
         {person.name}<br />
-        <img
-            src={`https://fdnd.directus.app/assets/${person.mugshot_year2}`}
-            alt={`Mugshot of ${person.name}`}
-            width="300"
-            height="300"
-        />
+        {#if person.mugshot_year2}
+            <img
+                src={`https://fdnd.directus.app/assets/${person.mugshot_year2}`}
+                alt={`Mugshot of ${person.name}`}
+                width="300"
+                height="300"
+            />
+        {:else}
+            <img
+                src={`https://fdnd.directus.app/assets/${person.mugshot}`}
+                alt={`Mugshot of ${person.name}`}
+            />
+        {/if}
     </a>
 {/each}
