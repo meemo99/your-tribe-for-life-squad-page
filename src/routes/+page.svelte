ac80f3908734b131bcab51f1d18809dc08bac619
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-11 20:25:13 +0200

    Fix: Fonts verbeterd en css verplaats naar een ander mapje #21
    Als feedback heb ik gekregen om de waarde van de fonts te veranderen naar `woff`of `woff2`. Ook om CSS toe te voegen aan `app.css`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script>
     let { data } = $props();
-
     const persons = $derived (data.persons);
 </script>
 
@@ -11,66 +10,3 @@
     <p>werkt de font en kleur?</p>
 {/each}
 
-<style>
-    :global(body) {
-        /* font */
-        /* Font H1 and all p */
-        --font-primary: 'Science Gothic';
-        /* Font for h2 */
-        --font-secondary: 'Ropa Sans';
-        /* Font for '(squad) 2G' */
-        --font-tertiary: 'Rock Salt';
-
-        /* Border-radius */ 
-        --border-radius-regular: 15px;
-        --border-radius-circel: 30px;
-
-        /* Colors */
-
-        /* Background */
-        --background-color-primary: #FECCDD;
-
-        /* Cards */
-        --color-card-pink: #EF78C9;
-        --color-card-team-pink: #F34CA6;
-
-        --color-card-purple: #9D96FF;
-        --color-card-team-purple: #6F66F1;
-
-        --color-card-orange: #FFB881;
-        --color-card-team-orange: #F08837;
-
-        --color-card-green: #CFEB79;
-        --color-card-team-green: #ADD72C;
-
-        --color-card-blue: #BDE3FF;
-        --color-card-team-blue: #70BFF8;
-
-        /* Texts */
-        --color-text-primary: #520931;
-        --color-text-secunday: #EE7BBE;
-        --color-text-accent: #F38AB8;
-
-        /* Nav buttons (Mac versie) */
-        --color-nav-green: #63C327;
-        --color-nav-red: #F74E4E;
-        --color-nav-orange: #F29955;
-    }
-
-    @font-face {
-        font-family: 'Science Gothic';
-        src: url('/fonts/ScienceGothic-VariableFont_CTRS,slnt,wdth,wght.ttf') format('truetype');
-    }
-    @font-face {
-        font-family: 'Rock Salt';
-        src: url('/fonts/RockSalt-Regular.ttf') format('truetype');
-    }
-    @font-face {
-        font-family: 'Ropa Sans';
-        src: url('/fonts/RopaSans-Italic.ttf') format('truetype');
-    }
-    p{
-        color: var(--color-text-secunday);
-        font-family: var(--font-primary);
-    }
-</style>
