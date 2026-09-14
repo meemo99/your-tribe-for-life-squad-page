628b660f4dcab9b185b2d1147d45a9f9ac68f617
Akikosophia <asschermer@hotmail.com>
2026-09-14 08:38:10 +0200

    refactor: improve members list semantics

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,35 @@
 <script>
-    let { data } = $props();
+  let { data } = $props();
 
-    const persons = $derived (data.persons);
+  const persons = $derived(data.persons);
 
-    import MemberCard from '$lib/components/MemberCard.svelte';
+  import MemberCard from "$lib/components/MemberCard.svelte";
 </script>
 
 <h1>Squadpage Squad G</h1>
 
-<div class="members">
-{#each persons as person}
-   <MemberCard {person} />
-{/each}
-</div>
+<ul class="members">
+  {#each persons as person}
+    <li>
+      <MemberCard {person} />
+    </li>
+  {/each}
+</ul>
 
 <style>
+  .members {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1rem;
+    padding: 1rem;
+    list-style: none;
+    margin: 0;
+  }
 
-.members {
- display: flex;
- flex-wrap: wrap;
- gap: 1rem;
- padding: 1rem;
- justify-content: center;
-}
-
-@media (min-width: 1024px) { 
+  @media (min-width: 1024px) {
     .members {
-  justify-content: center;
+      justify-content: center;
+    }
   }
-  }
-
-
 </style>
