e3521b0c2a4096a12f2d82a54ff3f5c508d7ec00
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-17 10:52:58 +0200

    Refactor: mugshot en github-handel toegevoegd #
    De mugshot is voor de img en de github-handel is voor de members die geeb profilecard habben dan zie je github-handel

---
+++ src/lib/components/DetailMemberCard.svelte
@@ -0,0 +1,6 @@
+        {#if person.profilecard }
+            <a class="project" href={person.profilecard}>Profilecard</a>
+            {:else}
+            <a class="project" href={`https://github.com/${person.github_handle}`}>Github</a>
+        {/if}
+    </section>
