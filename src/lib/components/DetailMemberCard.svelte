f070a9716741125e03ca3dbbaa3b877de2f1f894
khiettt <khitam.bakkali.nebkhout@hva.nl>
2026-09-17 10:55:04 +0200

    Feat: Html toegevoegd
    Bij alle members zie je de mugshot, naam, bio en/of profilecard/githubhandel

--- src/lib/components/DetailMemberCard.svelte
+++ src/lib/components/DetailMemberCard.svelte
@@ -1,6 +1,30 @@
+<script>
+  let { person } = $props();
+</script>
+<header>
+    <p class="fdnd">FDND</p>
+    <a href="/" aria-label="Terug naar overzicht" class="back-btn"> Terug naar overzicht</a>
+</header>
+
+<div>
+    <img
+        src={`https://fdnd.directus.app/assets/${person.mugshot_year2}`}
+        alt=""
+        width="250"
+        height="300"
+    />
+    <section>
+        <h2>{person.name}</h2>
+
+        <p class="bio-detail">{@html person.bio}</p>
+
         {#if person.profilecard }
             <a class="project" href={person.profilecard}>Profilecard</a>
             {:else}
             <a class="project" href={`https://github.com/${person.github_handle}`}>Github</a>
         {/if}
     </section>
+</div>
+
+
+
